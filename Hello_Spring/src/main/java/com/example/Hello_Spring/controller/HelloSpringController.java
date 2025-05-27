f3c801db05f3e4bd44f8ffd0268098a0ab42b019
Buicongbang04 <buicongbang1010@gmail.com>
2025-05-27 22:32:05 +0700

    controller hello

---
+++ Hello_Spring/src/main/java/com/example/Hello_Spring/controller/HelloSpringController.java
@@ -0,0 +1,13 @@
+package com.example.Hello_Spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// @RestController: use to notify to spring this is the controller
+@RestController
+public class HelloSpringController {
+    @GetMapping("/hello")
+    String sayHello(){
+        return "Hello Spring!";
+    }
+}
