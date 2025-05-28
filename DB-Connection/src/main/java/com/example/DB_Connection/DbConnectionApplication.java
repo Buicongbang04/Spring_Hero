9642d8856ae504459ade21d7eacff624300737db
Buicongbang04 <buicongbang1010@gmail.com>
2025-05-28 17:55:47 +0700

    connect db

---
+++ DB-Connection/src/main/java/com/example/DB_Connection/DbConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.DB_Connection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbConnectionApplication.class, args);
+	}
+
+}
---
+++ DB-Connection/src/test/java/com/example/DB_Connection/DbConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DB_Connection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
