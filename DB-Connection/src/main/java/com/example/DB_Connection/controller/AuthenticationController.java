761392390251c193b72de1811f7d6489c66b1ccb
Buicongbang04 <buicongbang1010@gmail.com>
2025-06-26 19:57:49 +0700

    add: JWT

--- DB-Connection/src/main/java/com/example/DB_Connection/controller/AuthenticationController.java
+++ DB-Connection/src/main/java/com/example/DB_Connection/controller/AuthenticationController.java
@@ -2,8 +2,11 @@ package com.example.DB_Connection.controller;
 
 import com.example.DB_Connection.dto.request.APIResponse;
 import com.example.DB_Connection.dto.request.AuthenticationRequest;
+import com.example.DB_Connection.dto.request.IntrospectRequest;
 import com.example.DB_Connection.dto.response.AuthenticationResponse;
+import com.example.DB_Connection.dto.response.IntrospectResponse;
 import com.example.DB_Connection.service.AuthenticationService;
+import com.nimbusds.jose.JOSEException;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -12,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
+
+import java.text.ParseException;
 
 @RestController
 @RequestMapping("/auth") // khởi tạo end-point
@@ -25,6 +30,14 @@ public class AuthenticationController {
     APIResponse<AuthenticationResponse> authenticateUser(@RequestBody AuthenticationRequest request) {
         var result = authenticationService.authenticate(request);
         return APIResponse.<AuthenticationResponse>builder()
+                .result(result)
+                .build();
+    }
+
+    @PostMapping("introspect")
+    APIResponse<IntrospectResponse> authenticateUser(@RequestBody IntrospectRequest request) throws ParseException, JOSEException {
+        var result = authenticationService.introspect(request);
+        return APIResponse.<IntrospectResponse>builder()
                 .result(result)
                 .build();
     }
--- DB-Connection/src/main/java/com/example/DB_Connection/service/AuthenticationService.java
+++ DB-Connection/src/main/java/com/example/DB_Connection/service/AuthenticationService.java
@@ -1,22 +1,28 @@
 package com.example.DB_Connection.service;
 
 import com.example.DB_Connection.dto.request.AuthenticationRequest;
+import com.example.DB_Connection.dto.request.IntrospectRequest;
 import com.example.DB_Connection.dto.response.AuthenticationResponse;
+import com.example.DB_Connection.dto.response.IntrospectResponse;
 import com.example.DB_Connection.exception.AppException;
 import com.example.DB_Connection.exception.ErrorCode;
 import com.example.DB_Connection.repository.UserRepository;
 import com.nimbusds.jose.*;
 import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.MACVerifier;
 import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.NonFinal;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
@@ -26,9 +32,25 @@ import java.util.Date;
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AuthenticationService {
-    @NonFinal
-    protected final String SIGNER_KEY = "D+F7gnK0ShUYubT3lblSG6dKOoYYKQcoip9XGfxKKML/b1Zx1Ylw7rt5qv/F+94I";
     UserRepository userRepository;
+
+    @NonFinal
+    @Value("${jwt.signerKey}")
+    protected String SIGNER_KEY;
+
+    public IntrospectResponse introspect(IntrospectRequest request) throws JOSEException, ParseException {
+        var token = request.getToken();
+
+        JWSVerifier verifier = new MACVerifier(SIGNER_KEY.getBytes());
+
+        SignedJWT signedJWT = SignedJWT.parse(token);
+
+        Date expiryTime = signedJWT.getJWTClaimsSet().getExpirationTime();
+
+        var verified = signedJWT.verify(verifier);
+
+        return IntrospectResponse.builder().valid(verified && expiryTime.after(new Date())).build();
+    }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
         var user = userRepository.findUserByUsername(request.getUsername())
